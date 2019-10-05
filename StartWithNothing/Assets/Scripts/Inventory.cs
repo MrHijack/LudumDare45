fa13ab13b07d4b2d82255079c3ac5d2dc1a5339c
Philip Sieberg <PhilipSieberg@users.noreply.github.com>
2019-10-05 16:22:34 +0200

    Ui Started

---
+++ StartWithNothing/Assets/Scripts/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private const string OPEN_ID = "Open";
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject toolbar;
+    [SerializeField] private GameObject inventory;
+
+    public bool Open { get; set; } = false;
+    public bool Busy;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void Toggle()
+    {
+        if (!Busy)
+        {
+            if (Open)
+            {
+                CloseInventory();
+            }
+            else
+            {
+                OpenInventory();
+            }
+        }
+    }
+
+    private void OpenInventory()
+    {
+        Open = true;
+        animator.SetBool(OPEN_ID, true);
+    }
+
+    private void CloseInventory()
+    {
+        Open = false;
+        animator.SetBool(OPEN_ID, false);
+    }
+}
---
+++ StartWithNothing/Assets/Scripts/UiManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class UiManager : MonoBehaviour
+{
+    public Inventory Inventory;
+    public FirstPersonController Player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && !Inventory.Busy)
+        {
+            Inventory.Toggle();
+            Player.ToggleMouseLock();
+        }
+    }
+}
