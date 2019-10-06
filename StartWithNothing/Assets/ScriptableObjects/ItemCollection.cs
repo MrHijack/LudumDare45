5893bed04f230939b1a71cb9f48f1c96e905d146
Philip Sieberg <PhilipSieberg@users.noreply.github.com>
2019-10-06 10:41:05 +0200

    Item Pickup

---
+++ StartWithNothing/Assets/ScriptableObjects/ItemCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemCollection", menuName = "ScriptableObjets/Item Collection")]
+public class ItemCollection : ScriptableObject
+{
+    public ItemAsset[] Items;
+}
---
+++ StartWithNothing/Assets/Scripts/ItemResolver.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ItemResolver : MonoBehaviour
+{
+    public static ItemResolver Instance { get; private set; }
+
+    public ItemCollection ItemCollection;
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+    public static Sprite ResolveSprite(Item? item)
+    {
+        return Instance.ResolveSpriteInternal(item);
+    }
+
+    private Sprite ResolveSpriteInternal(Item? item)
+    {
+        if (item != null)
+        {
+            return GetItemAsset(item.Value)?.Icon;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static GameObject ResolvePrefab(Item? item)
+    {
+        return Instance.ResolvePrefabInternal(item);
+    }
+
+    private GameObject ResolvePrefabInternal(Item? item)
+    {
+        if (item != null)
+        {
+            return GetItemAsset(item.Value)?.Prefab;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private ItemAsset GetItemAsset(Item item)
+    {
+        return ItemCollection.Items.Where(entry => entry.Id == item.Id).FirstOrDefault();
+    }
+
+}
